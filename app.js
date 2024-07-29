5e9857573f5b72f32db6e5e606561e6bef335a94
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-07-30 01:40:44 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// function checkWord() {
+//   let user_input = document.getElementById("userInput").value;
+//   let reverse = "";
+//   if (user_input == "") {
+//     alert("Plz Enter Any Word");
+//   } else {
+//     for (var i = user_input.length - 1; i >= 0; i--) {
+//       reverse += user_input[i];
+//     }
+
+//     if (user_input === reverse) {
+//       let list = document.getElementById("list");
+//       list.innerHTML = `${user_input} is a Palindrome Word`;
+//     } else {
+//       list.innerHTML = `${user_input} is not a Palindrome Word`;
+//     }
+//   }
+
+//   return false;
+// }
+
+// var input = prompt("enter");
+// for(var i=input.length -1;i>=0;i--){
+//   console.log(input[i]); 
+// }
